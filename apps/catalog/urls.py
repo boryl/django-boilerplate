ed121f0864ec8fa1ce5732e0556012f41f578125
boryl <bjornolle.rylander@gmail.com>
2020-01-21 14:54:10 +0100

    forms boilerplate

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -23,4 +23,24 @@ urlpatterns = [
         views.LoanedBooksListView.as_view(),
         name='all-borrowed'
     ),
+    path(
+        'book/<uuid:pk>/renew/',
+        views.renew_book_librarian,
+        name='renew-book-librarian'
+    ),
+    path(
+        'author/create/',
+        views.AuthorCreate.as_view(),
+        name='author_create'
+    ),
+    path(
+        'author/<int:pk>/update/',
+        views.AuthorUpdate.as_view(),
+        name='author_update'
+    ),
+    path(
+        'author/<int:pk>/delete/',
+        views.AuthorDelete.as_view(),
+        name='author_delete'
+    ),
 ]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,9 +1,19 @@
+import datetime
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, PermissionRequiredMixin
 )
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Book, Author, BookInstance, Genre
+from .forms import RenewBookForm
 
 
 class AuthorListView(generic.ListView):
@@ -80,3 +90,61 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'catalog/index.html', context=context)
+
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate
+        # it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (
+            # here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['due_back']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('catalog:all-borrowed'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_back_date =\
+            datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'due_back': proposed_back_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    login_url = '/accounts/login/'
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    login_url = '/accounts/login/'
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    login_url = '/accounts/login/'
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    success_url = reverse_lazy('catalog:authors')
